a0ea35b55b487f55a43d755c7df76fac3ef215cf
David Eisinger <david.eisinger@viget.com>
2008-10-21 10:15:35 -0400

    Updated to work w/ latest EmailLabs authentication

--- email_labs_client.rb
+++ email_labs_client.rb
@@ -1,48 +1,101 @@
 class EmailLabsClient
-  ENDPOINT      = 'http://www.elabs7.com/API/mailing_list.html'
-  SITE_ID       = '1234567890'
-  MAILING_LISTS = { 
-    'site_updates' => { :id => '12345', :trigger => '6789' },
-    'newsletter'   => { :id => '24560' }
+  ENDPOINT    = 'http://www.elabs7.com/API/mailing_list.html'
+  SITE_ID     = '1234567890'
+  MLID        = '12345'
+  PASSWORD    = 'xxxxxxx'
+  EMAIL_TYPES = {
+    :newsletter   => { :demographic => '12345', :trigger => '1234' },
+    :notification => { :demographic => '23456', :trigger => '2345' },
+    :invitation   => { :demographic => ['34567', '45678'], :trigger => '3456' }
   }
-  
+
   def self.method_missing(symbol, *args)
     case symbol.to_s
     when /subscribe_user_to_(\w+)/
-      subscribe_user MAILING_LISTS[$1][:id], *args
+      subscribe_user EMAIL_TYPES[$1.singularize.to_sym][:demographic], *args
+    when /unsubscribe_user_from_(\w+)/
+      unsubscribe_user EMAIL_TYPES[$1.singularize.to_sym][:demographic], *args
+    when /(\w+)_has_subscriber\?/
+      has_subscriber? EMAIL_TYPES[$1.singularize.to_sym][:demographic], *args
     when /send_(\w+)/
-      send_email MAILING_LISTS[$1][:id], MAILING_LISTS[$1][:trigger], *args
+      send_email EMAIL_TYPES[$1.to_sym][:trigger], *args
+    else
+      super
     end
   end
-  
-  protected
-  
-  def self.subscribe_user(mailing_list, email_address, options = {})
-    send_request('record', 'add') do |body|
-      body.MLID mailing_list
+
+  def self.user_exists?(email_address)
+    send_request('record', 'query-data') do |body|
       body.DATA email_address, :type => 'email'
+    end
+  end
+
+  protected
+
+  def self.subscribe_user(demographic_ids, email_address, options = {})
+    demographic_ids = [demographic_ids] if demographic_ids.is_a?(String)
+    action = user_exists?(email_address) ? 'update' : 'add'
+
+    send_request('record', action) do |body|
+      body.DATA email_address, :type => 'email'
+      demographic_ids.each do |demographic_id|
+        body.DATA 'on', :type => 'demographic', :id => demographic_id
+      end
       body.DATA options[:first_name], :type => 'demographic', :id => '1' unless options[:first_name].blank?
       body.DATA options[:last_name],  :type => 'demographic', :id => '2' unless options[:last_name].blank?
     end
   end
-  
-  def self.send_email(mailing_list, trigger_id, email_address, message)
+
+  def self.unsubscribe_user(demographic_ids, email_address, options = {})
+    demographic_ids = [demographic_ids] if demographic_ids.is_a?(String)
+    send_request('record', 'update') do |body|
+      body.DATA email_address, :type => 'email'
+      demographic_ids.each do |demographic_id|
+        body.DATA nil, :type => 'demographic', :id => demographic_id
+      end
+    end
+  end
+
+  def self.has_subscriber?(demographic_ids, email_address, options = {})
+    demographic_ids = [demographic_ids] if demographic_ids.is_a?(String)
+    response = send_request('record', 'query-data', true) do |body|
+      body.DATA email_address, :type => 'email'
+    end
+    demographic_ids.all? do |d|
+      response.include? "<DATA type=\"demographic\" id=\"#{d}\">on</DATA>"
+    end
+  end
+
+  def self.send_email(trigger_id, email_address, message, options = {})
     send_request('triggers', 'fire-trigger') do |body|
-      body.MLID mailing_list
       body.DATA trigger_id,    :type => 'extra', :id => 'trigger_id'
       body.DATA email_address, :type => 'extra', :id => 'recipients'
       body.DATA message,       :type => 'extra', :id => 'message'
-    end    
+      options.to_a.sort_by {|o| o.first.to_s }.each do |key, value|
+        body.DATA value, :type => 'extra', :id => key.to_s unless value.blank?
+      end
+    end
   end
-  
-  def self.send_request(request_type, activity)
+
+  def self.send_request(type, activity, return_body = false)
     xml = Builder::XmlMarkup.new :target => (input = '')
-    xml.instruct!
     xml.DATASET do
       xml.SITE_ID SITE_ID
+      xml.MLID MLID
+      xml.DATA PASSWORD, :type => 'extra', :id => 'password'
       yield xml
     end
-    Net::HTTP.post_form(URI.parse(ENDPOINT), :type => request_type, :activity => activity, :input => input)
+
+    conn = Net::HTTP.new("www.elabs7.com", 443)
+    conn.use_ssl = true
+    conn.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    response = conn.start do |http|
+      req = Net::HTTP::Post.new("/API/mailing_list.html")
+      req.set_form_data("activity" => activity, "type" => type, "input" => input)
+      http.request(req).body
+    end
+    return_body ? response : response.include?("<TYPE>success</TYPE>")
   end
 
 end
--- email_labs_client_test.rb
+++ email_labs_client_test.rb
@@ -11,51 +11,67 @@ require 'active_support'
 require File.dirname(__FILE__) + '/email_labs_client'
 
 class EmailLabsClientTest < Test::Unit::TestCase
-  
   context "The EmailLabsClient class" do
-  
-    should "should add a new user" do
-      Net::HTTP.expects(:post_form).with(URI.parse(EmailLabsClient::ENDPOINT),
-        :type => 'record', :activity => 'add',
-        :input => "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-                  "<DATASET>" +
-                    "<SITE_ID>#{EmailLabsClient::SITE_ID}</SITE_ID>" +
-                    "<MLID>#{EmailLabsClient::MAILING_LISTS['site_updates'][:id]}</MLID>" +
-                    "<DATA type=\"email\">user@example.com</DATA>" +
-                  "</DATASET>")
-      EmailLabsClient.subscribe_user_to_site_updates('user@example.com')
+    setup do
+      @messages = EmailLabsClient::EMAIL_TYPES[:site_message]
     end
     
-    should "should accept an optional name" do
-      Net::HTTP.expects(:post_form).with(URI.parse(EmailLabsClient::ENDPOINT),
-        :type => 'record', :activity => 'add',
-        :input => "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-                  "<DATASET>" +
-                    "<SITE_ID>#{EmailLabsClient::SITE_ID}</SITE_ID>" +
-                    "<MLID>#{EmailLabsClient::MAILING_LISTS['site_updates'][:id]}</MLID>" +
-                    "<DATA type=\"email\">user@example.com</DATA>" +
-                    "<DATA type=\"demographic\" id=\"1\">Sample</DATA>" + 
-                    "<DATA type=\"demographic\" id=\"2\">User</DATA>" +
-                  "</DATASET>")
-      EmailLabsClient.subscribe_user_to_site_updates('user@example.com',
-        :first_name => 'Sample', :last_name => 'User')
+    context "managing list membership" do
+      should "tell if a user is already on mailing list" do
+        EmailLabsClient.expects(:send_request).with('record',
+          'query-data').returns(true)
+        assert EmailLabsClient.user_exists?('user@example.com')
+      end
+
+      should "tell if a user is subscribed to an email type" do
+        EmailLabsClient.expects(:send_request).with('record',
+          'query-data', true).returns("<DATA type=\"demographic\" " +
+          "id=\"#{@messages[:demographic]}\">on</DATA>")
+        assert EmailLabsClient.site_messages_has_subscriber?('user@example.com')
+      end
+
+      should "add a new user" do
+        EmailLabsClient.expects(:user_exists?).with('user@example.com').returns false
+        EmailLabsClient.expects(:send_request).with('record',
+          'add').returns(true)
+        assert EmailLabsClient.subscribe_user_to_site_messages('user@example.com')
+      end
+
+      should "accept an optional name" do
+        EmailLabsClient.expects(:user_exists?).with('user@example.com').returns false
+        EmailLabsClient.expects(:send_request).with('record',
+          'add').returns(true)
+        assert EmailLabsClient.subscribe_user_to_site_messages('user@example.com',
+          :first_name => 'Sample', :last_name => 'User')
+      end
+    
+      should "update a user if already exists" do
+        EmailLabsClient.expects(:user_exists?).with('user@example.com').returns true
+        EmailLabsClient.expects(:send_request).with('record',
+          'update').returns(true)
+        assert EmailLabsClient.subscribe_user_to_site_messages('user@example.com')
+      end
+
+      should "remove a user" do
+        EmailLabsClient.expects(:send_request).with('record',
+          'update').returns(true)
+        assert EmailLabsClient.unsubscribe_user_from_site_messages('user@example.com')
+      end
     end
     
-    should "should send email" do
-      Net::HTTP.expects(:post_form).with(URI.parse(EmailLabsClient::ENDPOINT),
-        :type => 'triggers', :activity => 'fire-trigger',
-        :input => "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-                  "<DATASET>" +
-                    "<SITE_ID>#{EmailLabsClient::SITE_ID}</SITE_ID>" +
-                    "<MLID>#{EmailLabsClient::MAILING_LISTS['site_updates'][:id]}</MLID>" +
-                    "<DATA type=\"extra\" id=\"trigger_id\">" + 
-                      "#{EmailLabsClient::MAILING_LISTS['site_updates'][:trigger]}</DATA>" +
-                    "<DATA type=\"extra\" id=\"recipients\">user@example.com</DATA>" +
-                    "<DATA type=\"extra\" id=\"message\">Why hello there!</DATA>" +
-                  "</DATASET>")
-      EmailLabsClient.send_site_updates('user@example.com', 'Why hello there!')
+    context "sending email" do
+      should "send email" do
+        EmailLabsClient.expects(:send_request).with('triggers',
+          'fire-trigger').returns(true)
+        assert EmailLabsClient.send_site_message('user@example.com', 'Why hello there!')
+      end
+
+      should "accept sender name and email address" do
+        EmailLabsClient.expects(:send_request).with('triggers',
+          'fire-trigger').returns(true)
+        assert EmailLabsClient.send_site_message('user@example.com', 'Why hello there!',
+          :first_name => "John", :last_name => "Smith")
+      end
     end
-    
   end
-  
 end
